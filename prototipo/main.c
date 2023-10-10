09315efea5133071671bf0d117db10a8a17355f0
Helio <heliohenrique3@gmail.com>
2023-10-10 15:14:31 -0300

    Protótipo

---
+++ prototipo/main.c
@@ -0,0 +1,29 @@
+
+#include <stdio.h>
+
+int main() {
+
+    int escolha;
+    char x;
+
+    while (1) {
+        printf("#### Aplicação de lista de compras com listas encadeadas #####\n");
+        printf("Menu:\n");
+        printf("0. Imprimir lista:\n");
+        printf("1. Criar nova lista\n\n");
+
+        printf("Digite a opção desejada:");
+        scanf("%d", &escolha);
+
+        if (escolha == 0) {
+            printf("TODO: IMPRIMIR LISTA!!!\n\n");
+        } else if (escolha == 1) {
+            printf("TODO: CRIA NOVA LISTA\n\n");
+        } 
+        // printf("Tecle qualquer tecla para continuar: ");
+        // scanf("%s",x);
+        printf("\n\n\n\n\n\n\n\n\n\n\n\n");
+    }
+
+    return 0;
+}
